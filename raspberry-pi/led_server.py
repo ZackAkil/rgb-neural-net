dad9268feef82e7917941ffe38dbb34e47b58efa
ZackAkil <zackakil94@gmail.com>
2017-04-29 14:39:35 +0100

    added batch led change request action

--- raspberry-pi/led_server.py
+++ raspberry-pi/led_server.py
@@ -14,20 +14,29 @@ def change_led():
     green = request.form['green']
     blue = request.form['blue']
     led_num = request.form['led_num']
+    send_led_change_command(red, green, blue, led_num)
+    return 'Done'
 
+@app.route('/change_leds', methods=['POST'])
+def change_leds():
+    data = request.get_json()
+    leds = data.get('leds')
+    for led in leds:
+        send_led_change_command(led['red'], led['green'], led['blue'], led['led_num'])
+    return 'Done'
+
+def send_led_change_command(red, green, blue, led_num):
     format_red = "%03d" % int(red)
     format_green = "%03d" % int(green)
     format_blue = "%03d" % int(blue)
     format_led_num = "%02d" % int(led_num)
 
     message_to_send = '{0}{1}{2}{3}\n'.format(format_green,
-                                                    format_red,
-                                                    format_blue,
-                                                    format_led_num)
-
+                                              format_red,
+                                              format_blue,
+                                              format_led_num)
     arduinoSerialData.write(bytes(message_to_send, 'UTF-8'))
 
-    return 'Done'
 
 
 if __name__ == '__main__':
